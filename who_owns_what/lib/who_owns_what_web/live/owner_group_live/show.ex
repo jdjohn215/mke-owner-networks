407b16f8fe071c88e7425add90cf62b56c67698b
Mitchell Henke <mitchell@mitchellhenke.com>
2023-09-10 16:17:53 -0500

    show svg

--- who_owns_what/lib/who_owns_what_web/live/owner_group_live/show.ex
+++ who_owns_what/lib/who_owns_what_web/live/owner_group_live/show.ex
@@ -10,12 +10,45 @@ defmodule WhoOwnsWhatWeb.OwnerGroupLive.Show do
 
   @impl true
   def handle_params(%{"id" => name}, _, socket) do
+    properties = Data.list_properties_by_owner_group_name(name)
+
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:owner_group, Data.get_owner_group_by_name(name))
-     |> assign(:properties, Data.list_properties_by_owner_group_name(name))}
+     |> assign(:properties, properties)
+     |> assign(:svg, nil)
+     |> assign_graphs(properties)}
+  end
+
+  @impl true
+  def handle_info({port, {:data, svg}}, socket) when is_port(port) do
+    Port.close(port)
+
+    svg =
+      String.replace(svg, ~r/(width|height)="\d+pt"/, "")
+      |> String.replace(~r/<!--.*-->/, "")
+
+    socket =
+      assign(socket, :svg_port, nil)
+      |> assign(:svg, svg)
+
+    {:noreply, socket}
   end
 
   defp page_title(:show), do: "Show Owner group"
+
+  defp assign_graphs(socket, properties) do
+    graph =
+      Enum.reduce(properties, Graph.new(type: :undirected), fn property, graph ->
+        Graph.add_edge(graph, property.owner_name_1, property.owner_address)
+      end)
+
+    {:ok, dot} = Graph.Serializers.DOT.serialize(graph)
+    port = Port.open({:spawn, "dot -Grankdir=LR -Tsvg"}, [:binary])
+    Port.command(port, dot)
+
+    assign(socket, :graph, graph)
+    |> assign(:svg_port, port)
+  end
 end
