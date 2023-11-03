01d5287a2f755b76fab9f72a01073a1c55c307f7
jdjohn215 <jdjohn215@gmail.com>
2023-11-03 15:46:31 -0500

    init

---
+++ processing-scripts/mprop-standardize-addresses.R
@@ -0,0 +1,73 @@
+rm(list = ls())
+
+library(tidyverse)
+library(tidygeocoder)
+
+mprop <- read_csv("data/mprop/ResidentialProperties_NotOwnerOccupied.csv")
+
+# addresses previously standardized by geocodio
+#   or standardization attempted and failed
+standardized.addresses <- read_csv("data/mprop/standardized-addresses.csv")
+
+# new addresses
+new.addresses <- mprop %>%
+  filter(! mprop_address_raw %in% standardized.addresses$mprop_address_raw) %>%
+  group_by(mprop_address_raw) %>%
+  summarise()
+
+if(nrow(new.addresses) > 0){
+  # try geocoding new addresses
+  new.addresses.geocodio <- geocode(.tbl = new.addresses,
+                                    address = mprop_address_raw,
+                                    full_results = TRUE,
+                                    method = "geocodio")
+  
+  # process geocoded addresses, creating standardized full address where
+  #   validation was successful
+  new.addresses.standardized <- new.addresses.geocodio %>%
+    # add commas where relevant
+    mutate(city2 = paste(",", address_components.city),
+           state2 = paste(",", address_components.state),
+           unittype2 = if_else(!is.na(address_components.secondaryunit), 
+                               paste(",", address_components.secondaryunit), 
+                               NA_character_)) %>%
+    # create combined address string, dropping NA variables
+    unite(col = "mprop_address", address_components.number, address_components.street,
+          unittype2, address_components.secondarynumber, city2, state2, 
+          address_components.zip,
+          na.rm = T, remove = FALSE, sep = " ") %>%
+    # replace standardized address w/original address as needed
+    mutate(mprop_address = str_replace_all(mprop_address, " ,", ","),
+           mprop_address = case_when(
+             accuracy < 0.9 ~ mprop_address_raw,
+             is.na(address_components.number) ~ mprop_address_raw,
+             is.na(address_components.city) ~ mprop_address_raw,
+             TRUE ~ str_to_upper(mprop_address)
+           ),
+           standardized = case_when(
+             accuracy < 0.9 ~ FALSE,
+             is.na(address_components.number) ~ FALSE,
+             is.na(address_components.city) ~ FALSE,
+             TRUE ~ TRUE
+           ))
+  
+  
+  # add new standardized addresses
+  standardized.addresses.updated <- bind_rows(standardized.addresses, 
+                                              new.addresses.standardized %>%
+                                                select(mprop_address, mprop_address_raw, standardized))
+} else {
+  standardized.addresses.updated <- standardized.addresses
+}
+
+
+table(standardized.addresses.updated$standardized)
+
+# add standardized addresses to mprop
+mprop.with.standardized <- mprop %>%
+  left_join(standardized.addresses.updated)
+write_csv(mprop.with.standardized, "data/mprop/ResidentialProperties_NotOwnerOccupied_StandardizedAddresses.csv")
+
+# save updated table of standardized addresses
+write_csv(standardized.addresses.updated, "data/mprop/standardized-addresses.csv")
+
---
+++ processing-scripts/wdfi-standardize-addresses.R
@@ -0,0 +1,100 @@
+rm(list = ls())
+
+library(tidyverse)
+library(tidygeocoder)
+
+wdfi <- read_csv("data/wdfi/wdfi-current-in-mprop.csv")
+wdfi.addresses <- wdfi %>%
+  select(entity_id, principal_office_raw, agent_raw) %>%
+  pivot_longer(cols = -entity_id, values_to = "wdfi_address_raw")
+
+# addresses previously standardized by geocodio
+#   or standardization attempted and failed
+standardized.addresses <- read_csv("data/wdfi/standardized-addresses.csv")
+
+# new addresses
+new.addresses <- wdfi.addresses %>%
+  filter(! wdfi_address_raw %in% standardized.addresses$wdfi_address_raw) %>%
+  group_by(wdfi_address_raw) %>%
+  summarise()
+
+if(nrow(new.addresses) > 0){
+  # try geocoding new addresses
+  new.addresses.geocodio <- geocode(.tbl = new.addresses,
+                                    address = wdfi_address_raw,
+                                    full_results = TRUE,
+                                    method = "geocodio")
+  
+  # process geocoded addresses, creating standardized full address where
+  #   validation was successful
+  new.addresses.standardized <- new.addresses.geocodio %>%
+    # add commas where relevant
+    mutate(city2 = paste(",", address_components.city),
+           state2 = paste(",", address_components.state),
+           unittype2 = if_else(!is.na(address_components.secondaryunit), 
+                               paste(",", address_components.secondaryunit), 
+                               NA_character_)) %>%
+    # create combined address string, dropping NA variables
+    unite(col = "wdfi_address", address_components.number, address_components.street,
+          unittype2, address_components.secondarynumber, city2, state2, 
+          address_components.zip,
+          na.rm = T, remove = FALSE, sep = " ") %>%
+    # replace standardized address w/original address as needed
+    mutate(wdfi_address = str_replace_all(wdfi_address, " ,", ","),
+           wdfi_address = case_when(
+             accuracy < 0.9 ~ wdfi_address_raw,
+             is.na(address_components.number) ~ wdfi_address_raw,
+             is.na(address_components.city) ~ wdfi_address_raw,
+             TRUE ~ str_to_upper(wdfi_address)
+           ),
+           standardized = case_when(
+             accuracy < 0.9 ~ FALSE,
+             is.na(address_components.number) ~ FALSE,
+             is.na(address_components.city) ~ FALSE,
+             TRUE ~ TRUE
+           ))
+  
+  
+  # add new standardized addresses
+  standardized.addresses.updated <- bind_rows(standardized.addresses, 
+                                              new.addresses.standardized %>%
+                                                select(wdfi_address, wdfi_address_raw, standardized))
+} else {
+  standardized.addresses.updated <- standardized.addresses
+}
+
+table(standardized.addresses.updated$standardized)
+
+################################################################################
+# add standardized addresses to wdfi
+wdfi.with.standardized <- wdfi.addresses %>%
+  inner_join(standardized.addresses.updated) %>%
+  select(entity_id, name, address = wdfi_address, standardized) %>%
+  mutate(name = str_remove(name, "_raw")) %>%
+  pivot_wider(names_from = name, values_from = c(address, standardized),
+              names_glue = "{name}_{.value}") %>%
+  inner_join(wdfi)
+
+################################################################################
+# identify all the addresses used by useless registered agents
+#   remove records where the PRINCIPAL OFFICE ADDRESS is the same as one of these
+# wdfi useless registered agents
+wdfi.agents.not.useful <- readxl::read_excel("data/munges/WDFI_notes.xlsx",
+                                             sheet = 1) %>%
+  mutate(registered_agent = str_replace_all(registered_agent, "\\s", " "))
+
+# wdfi addresses used by useless registered agents
+wdfi.addresses.not.useful <- wdfi.with.standardized %>%
+  filter(registered_agent %in% wdfi.agents.not.useful$registered_agent |
+           str_detect(registered_agent, "\\bLAW\\b|\\bLAWYERS\\b|\\bLAWYER\\b|\\bATTORNEY\\b|\\TAX\\b|\\bACCOUNTING\\b|\\bINCORPORATING\\b|\\bAGENT\\b|\\bAGENTS\\b|CORPORATE SERV")) %>%
+  group_by(agent_address) %>%
+  summarise()
+
+useful.wdfi.with.standardized <- wdfi.with.standardized %>%
+  filter(! principal_office_address %in% wdfi.addresses.not.useful$agent_address)
+
+
+write_csv(useful.wdfi.with.standardized, "data/wdfi/wdfi-current-in-mprop_StandardizedAddresses.csv")
+
+# save updated table of standardized addresses
+write_csv(standardized.addresses.updated, "data/wdfi/standardized-addresses.csv")
