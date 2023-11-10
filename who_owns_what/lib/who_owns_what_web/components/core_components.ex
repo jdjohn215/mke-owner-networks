ed9d09dc80dacb55978796742a043e24e877e5c3
Mitchell Henke <mitchell@mitchellhenke.com>
2023-11-10 15:28:29 -0600

    new content

--- who_owns_what/lib/who_owns_what_web/components/core_components.ex
+++ who_owns_what/lib/who_owns_what_web/components/core_components.ex
@@ -422,7 +422,7 @@ defmodule WhoOwnsWhatWeb.CoreComponents do
     ~H"""
     <header class={[@actions != [] && "flex items-center justify-between gap-6", @class]}>
       <div>
-        <h1 class="text-lg font-semibold leading-8 text-zinc-800">
+        <h1 class="text-2xl font-semibold leading-8 text-zinc-800">
           <%= render_slot(@inner_block) %>
         </h1>
         <p :if={@subtitle != []} class="mt-2 text-sm leading-6 text-zinc-600">
