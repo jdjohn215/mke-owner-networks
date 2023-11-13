37018889cfd58ed514b33fc9fd9a06c008da4858
jdjohn215 <jdjohn215@gmail.com>
2023-11-13 15:54:25 -0600

    change stage at which current WDFI is subsetted

--- processing-scripts/Process-WDFI-Principal-Address.R
+++ processing-scripts/Process-WDFI-Principal-Address.R
@@ -2,7 +2,37 @@ rm(list = ls())
 
 library(tidyverse)
 
-wdfi <- vroom::vroom("data/wdfi/corpdata results Marquette_fixed.txt")
+wdfi.orig <- vroom::vroom("data/wdfi/corpdata results Marquette_fixed.txt")
 
-wdfi.processed <- wdfi
-write_csv(wdfi.processed, "data/wdfi/WDFI_PrincipalAddress_Processed.csv.gz")
+################################################################################
+# subset CURRENT WDFI
+wdfi.processed <- wdfi.orig %>%
+  janitor::clean_names() %>%
+  mutate(across(.cols = where(is.character),
+                .fns = ~if_else(.x %in% c("NULL", "XX"), NA, .x)))
+
+clean.names <- wdfi.processed %>%
+  # apply same cleaning routine as applied to OWNER_NAME_1
+  mutate(corp_name_clean = str_remove_all(entity_name, ","),
+         corp_name_clean = str_remove_all(corp_name_clean, coll(".")),
+         corp_name_clean = str_remove_all(corp_name_clean, "#"),
+         corp_name_clean = str_replace(corp_name_clean, " - ", "-"),
+         corp_name_clean = str_squish(corp_name_clean),
+         corp_name_clean = str_replace(corp_name_clean, "\\bLL$", "LLC"),
+         registered_agent = str_squish(str_to_upper(str_remove_all(agent_name, "[.]|[,]"))))
+
+# these entities are currently incorporated
+#   this includes entities in bad standing or delinquent but NOT YET terminated
+wdfi.current <- clean.names %>%
+  filter(entity_status %in% c("ORG", "IGS", "DLQ", "INC", "RGD", "IBS", "RLT")) %>%
+  # a small number of the cleaned names have duplicates
+  #   corporation names are required to be unique
+  #   this keeps just the most recently incorporated of the two identically-named companies
+  group_by(entity_id) %>%
+  filter(row_number() == 1) %>%
+  ungroup() %>%
+  group_by(corp_name_clean) %>%
+  slice_max(order_by = incorporated_date, n = 1, with_ties = FALSE) %>%
+  ungroup()
+
+write_csv(wdfi.current, "data/wdfi/WDFI_PrincipalAddress_Current.csv.gz")
