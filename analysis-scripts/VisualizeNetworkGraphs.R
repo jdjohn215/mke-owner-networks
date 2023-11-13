5af9cabbd67bfa4155269cb9bc11f73a5e0f2c7c
jdjohn215 <jdjohn215@gmail.com>
2023-11-13 14:57:43 -0600

    correct frequency measure of mprop_address-to-wdfi_address matches

--- analysis-scripts/VisualizeNetworkGraphs.R
+++ analysis-scripts/VisualizeNetworkGraphs.R
@@ -12,11 +12,13 @@ owner.networks <- df %>%
   summarise(parcels = n(),
             units = sum(NR_UNITS),
             names = paste(unique(mprop_name), collapse = "; "),
-            name_count = n_distinct(mprop_name))
+            name_count = n_distinct(mprop_name)) %>%
+  arrange(desc(parcels))
 
 ###############################################################################
 
-visualize_network_graph <- function(data, final_group_name, layout = "lgl", seed = 42){
+visualize_network_graph <- function(data, final_group_name, layout = "lgl", 
+                                    seed = 42, fontsize = 2){
   
   # subset the parcels for this network
   parcels <- data %>%
@@ -26,8 +28,8 @@ visualize_network_graph <- function(data, final_group_name, layout = "lgl", seed
   addresses.in.both <- parcels %>%
     filter(str_sub(mprop_address, 1, -7) %in% str_sub(wdfi_address, 1, -6)) %>%
     mutate(matched_address = str_sub(mprop_address, 1, -7)) %>%
-    pull(matched_address) %>%
-    unique()
+    group_by(matched_address) %>%
+    summarise()
   
   # create all node connections
   #   * mprop name TO mprop address
@@ -40,10 +42,22 @@ visualize_network_graph <- function(data, final_group_name, layout = "lgl", seed
     select(from = mprop_name, to = address) %>%
     # add connections which are MPROP address to WDFI address
     bind_rows(
-      tibble(
-        from = stringi::stri_join(paste0(addresses.in.both, "_mprop")),
-        to =  stringi::stri_join(addresses.in.both, "_wdfi")
-      )
+      parcels %>%
+        select(mprop_address, wdfi_address) %>%
+        pivot_longer(cols = everything(), values_to = "address") %>%
+        filter(!is.na(address)) %>%
+        mutate(matched_address = str_remove(address, "_mprop|_wdfi")) %>%
+        inner_join(addresses.in.both, by = join_by(matched_address)) %>%
+        group_by(name, matched_address) %>%
+        mutate(count = n()) %>%
+        ungroup() %>%
+        group_by(matched_address) %>%
+        arrange(desc(count), name) %>%
+        filter(name == first(name)) %>%
+        ungroup() %>%
+        mutate(to = paste0(matched_address, "_mprop"),
+               from = paste0(matched_address, "_wdfi")) %>%
+        select(to, from)
     )
   
   # frequency of each node
@@ -62,15 +76,15 @@ visualize_network_graph <- function(data, final_group_name, layout = "lgl", seed
       name %in% parcels$mprop_address ~ "mprop address",
       name %in% parcels$wdfi_address ~ "wdfi address"
     )) %>%
-    inner_join(node.frequency)
+    inner_join(node.frequency, by = join_by(name))
   
   # control randomness of graph layout
   set.seed(seed)
   ggraph(graph, layout = layout) + 
-    geom_edge_link() + 
+    geom_edge_fan(edge_alpha = 0.1) +
     geom_node_point(aes(color = node_class, shape = node_class, size = node_frequency)) +
     geom_node_label(aes(label = str_wrap(str_remove(name, "_mprop|_wdfi"), 20)), 
-                    size = if_else(nrow(node.frequency) > 75, 1.5, 2),
+                    size = fontsize,
                     repel = T, segment.color = "gray75", min.segment.length = 0.25,
                     segment.size = 0.25,
                     fill = alpha(c("white"),0.5), max.overlaps = 50) +
