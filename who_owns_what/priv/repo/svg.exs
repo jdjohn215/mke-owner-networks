7e10212ca1bffe95cdca7c4aef9492dd839b55cc
Mitchell Henke <mitchell@mitchellhenke.com>
2023-11-22 19:31:38 -0600

    svg update

--- who_owns_what/priv/repo/svg.exs
+++ who_owns_what/priv/repo/svg.exs
@@ -1,2 +1,4 @@
 destination = Path.join([:code.priv_dir(:who_owns_what), "static", "images", "networks"])
+System.cmd("rm", ["-r", destination])
+System.cmd("mkdir", ["-p", destination])
 System.cmd("cp", ["-r", "./../images/networks-svg/.", destination])
