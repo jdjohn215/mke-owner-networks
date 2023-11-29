40bc21752cb1c4baad58afec7a350208a28bf758
Mitchell Henke <mitchell@mitchellhenke.com>
2023-11-28 20:21:56 -0600

    add summary statistics

--- who_owns_what/lib/who_owns_what_web/live/home_live/index.ex
+++ who_owns_what/lib/who_owns_what_web/live/home_live/index.ex
@@ -1,9 +1,39 @@
 defmodule WhoOwnsWhatWeb.HomeLive.Index do
   use WhoOwnsWhatWeb, :live_view
 
+  @path Application.compile_env(:who_owns_what, :data_folder_path)
+  @summary_data_path Path.join([@path, "overall-summary-stats.csv"])
+  @external_resource @summary_data_path
+  @summary_data File.read!(@summary_data_path)
+                |> NimbleCSV.RFC4180.parse_string(skip_headers: true)
+                |> Enum.reduce(%{}, fn [key, value], map ->
+                  Map.put(map, key, value)
+                end)
+
   @impl true
   def mount(_params, _session, socket) do
-    socket = assign(socket, :page_title, "The Milwaukee Landlord Network Project")
+    socket =
+      assign(socket, :page_title, "The Milwaukee Landlord Network Project")
+      |> assign(:total_parcels, Map.fetch!(@summary_data, "total_parcels"))
+      |> assign(:total_mprop_names, Map.fetch!(@summary_data, "total_mprop_names"))
+      |> assign(:total_mprop_addresses, Map.fetch!(@summary_data, "total_mprop_addresses"))
+      |> assign(:total_networks, Map.fetch!(@summary_data, "total_networks"))
+      |> assign(
+        :pct_networks_single_parcel,
+        Map.fetch!(@summary_data, "pct_networks_single_parcel")
+      )
+      |> assign(
+        :pct_networks_multiple_names,
+        Map.fetch!(@summary_data, "pct_networks_multiple_names")
+      )
+      |> assign(
+        :pct_parcels_multiple_name_owner,
+        Map.fetch!(@summary_data, "pct_parcels_multiple_name_owner")
+      )
+      |> assign(:mprop_updated, Map.fetch!(@summary_data, "mprop_updated"))
+      |> assign(:workflow_updated, Map.fetch!(@summary_data, "workflow_updated"))
+      |> assign(:wdfi_updated, Map.fetch!(@summary_data, "wdfi_updated"))
+
     {:ok, socket}
   end
 end
