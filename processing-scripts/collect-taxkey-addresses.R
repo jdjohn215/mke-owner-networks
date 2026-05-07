36a3b11fc387f7a63f7139400977d3cee0e42b7e
Mitchell Henke <mitchell@mitchellhenke.com>
2026-05-06 19:46:35 -0500

    include unit number in MAI addresses

--- processing-scripts/collect-taxkey-addresses.R
+++ processing-scripts/collect-taxkey-addresses.R
@@ -8,7 +8,7 @@ library(tidyverse)
 mai <- read_csv("https://data.milwaukee.gov/dataset/566af1a6-0499-4766-a89e-2f2a4b4d6e2d/resource/9f905487-720e-4f30-ae70-b5ec8a2a65a1/download/mai.csv")
 
 taxkey.addresses <- mai |>
-  unite("address_string", HSE_NBR, SFX, DIR, STREET, STTYPE, na.rm = T, sep = " ") |>
+  unite("address_string", HSE_NBR, SFX, DIR, STREET, STTYPE, UNIT_NBR, na.rm = T, sep = " ") |>
   group_by(TAXKEY) |>
   summarise(complete_addresses = paste(unique(address_string), collapse = "|"))
 
